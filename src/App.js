d2e52b790a34d1f4aa0a78c6cebf961f75e45302
chilam0103 <chilam9x@gmail.com>
2020-05-18 16:51:01 +0700

    nav link

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import NotFound from "./components/NotFound";
+import Menu from "./components/Menu";
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+        {/* menu */}
+          <Menu />
+          {/*noi dung ---exact là đường dẫn chính xác*/}
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/contact" component={Contact} />
+            <Route component={NotFound} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React,{Component} from "react";
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="">trang chủ</div>
+    );
+  }
+}
+
+export default Home;
