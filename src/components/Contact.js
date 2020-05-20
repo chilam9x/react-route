40e695496efafd680db503f00366a3d5c5a7f581
chilam0103 <chilam9x@gmail.com>
2020-05-20 13:55:55 +0700

    đối tượng prompt-ngăn cản chuyển trang

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,45 @@
-import React,{Component} from "react";
+import React, { Component } from "react";
+import { Prompt } from "react-router-dom";
 
 class Contact extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isChecked: false,
+    };
+  }
+  onClick = (isChecked) => {
+    this.setState({
+      isChecked: isChecked,
+    });
+  };
   render() {
+    var {isChecked} = this.state;
     return (
-      <div className="">liên hệ</div>
+      <div>
+        <button
+          className="btn btn-primary"
+          type="button"
+          onClick={() => this.onClick(false)}
+        >
+          Cho Phép
+        </button>
+        <br />
+        <button
+          className="btn btn-danger"
+          type="button"
+          onClick={() => this.onClick(true)}
+        >
+          Không Cho Phép
+        </button>
+        <Prompt
+          when={isChecked}
+          message={location =>
+           ( `Bạn có chắc chắn muốn đi đến ${location.pathname}`)
+          }
+        />
+        <br />
+      </div>
     );
   }
 }
